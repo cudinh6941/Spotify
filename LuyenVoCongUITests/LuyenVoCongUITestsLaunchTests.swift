7ad947ff94f770c458109aab259a9baeeab03849
Dinh Pham Kha <dinh.pk@aris-vn.com>
2023-08-28 16:18:29 +0700

    add some code

---
+++ LuyenVoCongUITests/LuyenVoCongUITestsLaunchTests.swift
@@ -0,0 +1,15 @@
+//
+//  LuyenVoCongUITestsLaunchTests.swift
+//  LuyenVoCongUITests
+//
+//  Created by Dinh Pham Kha on 28/08/2023.
+//
+
+import XCTest
+
+final class LuyenVoCongUITestsLaunchTests: XCTestCase {
+    func testTappedItem(){
+ 
+            
+    }
+}
